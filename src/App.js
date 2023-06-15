5cf510f185d439036ce1f271fa09a32ec9204cb3
Eugene Shih <eugene.shih.1@gmail.com>
2023-06-15 16:02:07 -0500

    Modify create-react-app baseline to mirror css project.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import './eugene-style.css';
+
 
 function App() {
   return (
+  <div class="content">
+      <p>
+        An enthusiast of simulations and other complicated contraptions. 
+        I play way too many videogames, have a desire to get into tabletop and try to stay up to date on computer programming skills.
+      </p>
+  </div>
+    /*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +28,7 @@ function App() {
         </a>
       </header>
     </div>
+    */
   );
 }
 
---
+++ src/Sidebar.js
@@ -0,0 +1,14 @@
+import './eugene-style.css';
+
+
+
+function Sidebar() {
+    var rootPage = "nullAurelian.github.io/scribble-pad";
+    return (
+        <div class="sidenav">
+                <a href="${rootPage}">Test</a>
+        </div>
+    )
+}
+
+export default Sidebar;
